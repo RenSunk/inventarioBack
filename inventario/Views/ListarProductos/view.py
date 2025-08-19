a75760dbdbbc92bc7d8edcab1f33567845d1ccb2
Kevin Rodrigo Guerrero Posso <kevin.guerrero@pacific-hs.com>
2025-08-18 20:26:08 -0500

    paginador

--- inventario/Views/ListarProductos/view.py
+++ inventario/Views/ListarProductos/view.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from inventario.models import *
 from inventario.Views.ListarProductos.serializers import ProductVariantSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -12,6 +13,7 @@ class LargeResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
 
+    
 class ListarProductos(APIView):
 
     authentication_classes = [JWTAuthentication]
@@ -29,11 +31,8 @@ class ListarProductos(APIView):
         paginated_queryset = paginator.paginate_queryset(productos, request)
 
         if paginated_queryset is not None:
-            serializer = ProductVariantSerializer(productos, many=True)
+            serializer = ProductVariantSerializer(paginated_queryset, many=True)
             return paginator.get_paginated_response(serializer.data)
         else:
             serializer = ProductVariantSerializer(productos, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-        return Response(serializer.data)
